ae2107779ad6d16ce75cdfe994008cc2dc2fe649
Akash Kamble <96697647+iamakash-ai@users.noreply.github.com>
2026-02-18 19:44:31 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,7 +85,7 @@ text-align: center;
 }
 </style>
 <div class="footer">
-  <p style="color: #000000; font-weight: bold;">Develop with <span style='color:red;'> ❤ </span> CodeWithSky </p>
+  <p style="color: #000000; font-weight: bold;">Developed with <span style='color:red;'> ❤ </span> CodeWithSky </p>
 </div>
 """
 st.markdown(footer,unsafe_allow_html=True)
